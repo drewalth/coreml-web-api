26155495e828b00d2d0f0807cb7ce531a9d56915
Drew <andrew.althage@gmail.com>
2023-11-03 17:59:52 -0600

    Initial commit

---
+++ CoreMLWebClient/CoreMLWebClient/CoreMLWebClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLWebClientApp.swift
+//  CoreMLWebClient
+//
+//  Created by Andrew Althage on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreMLWebClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
